5a4985803581b0c7074417f1b3979a7c481f87f7
Enes Bek <enesbek@Enes-MacBook-Air.local>
2023-01-02 00:37:39 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ethers } from "ethers";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [account, setAccount] = useState("isn't connected");
+
+  const connectWallet = async () => {
+    if (!window.ethereum) {
+      window.open("https://metamask.io/download/");
+    } else {
+      let accounts = await window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .catch((err) => {
+          console.error(err);
+        });
+
+      setAccount(accounts[0]);
+      const token = await signWallet();
+
+      axios.post("/registration", {
+        headers: {
+          Authorization: token,
+        },
+      });
+    }
+  };
+
+  const signWallet = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+
+    const signedMessage = await signer
+      .signMessage("Message: This should be nonce value")
+      .catch((err) => {
+        console.error(err);
+      });
+
+    return signedMessage;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Wallet addr: {account}</div>
+      <button style={{ marginTop: "20px" }} onClick={connectWallet}>
+        Connect Wallet
+      </button>
     </div>
   );
 }
